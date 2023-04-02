a1ca22e8dfada33e8537a9f23d0cdba13690d070
Domisiaxa <domisiaxa@gmail.com>
2023-04-02 15:12:51 +0200

    G

---
+++ src/components/services/settings.js
@@ -0,0 +1,8 @@
+const settings = {
+  BASE_URL: 'https://pixabay.com/api/?',
+  API_KEY: 'key=32928736-f14b72ba40513289fec4fb952',
+  TYPE: '&image_type=photo',
+  PER_PAGE: '&per_page=12',
+};
+
+export default settings;
